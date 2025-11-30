751618c3c5a4b96457b352e264aa3d3c7f44be74
AtakyRevolts <karlos_wiliam@hotmail.com>
2025-11-30 00:39:59 -0300

    build: preparing environment to multi-platform building

---
+++ FeedTheSea/build.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    name = 'Feed The Sea',
+    config = 'nonsteam',
+    version = '0.1.0',
+    love = '11.5',
+    icon = 'icon.png',
+    platforms = {'windows', 'linux'},
+    ignore = {'tests', '.gitignore', 'README.md', 'setup_project.sh'}
+  }
+}
