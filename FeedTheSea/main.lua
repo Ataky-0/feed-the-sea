c898bb7f2e189f38eafe4206fc65a86c11d3ec2d
AtakyRevolts <karlos_wiliam@hotmail.com>
2025-12-08 17:33:47 -0300

    feat: added a minimum width x height to the window (that will be reworked for sure)

--- FeedTheSea/main.lua
+++ FeedTheSea/main.lua
@@ -2,6 +2,7 @@
 -- Leia sceneManager.lua para entender como as cenas funcionam
 local sceneManager = require("src.sceneManager")
 local timerManager = require("src.timerManager")
+local UI = require("src.ui")
 
 function love.quit()
 	local current = sceneManager.current
@@ -12,6 +13,8 @@ end
 
 function love.load()
 	sceneManager:changeScene("mainmenu")
+	local w, h = love.graphics.getDimensions()
+	love.window.setMode(w, h, {resizable = true, minwidth = UI.UI_BASE_W, minheight = UI.UI_BASE_H})
 	love.keyboard.setKeyRepeat(true) -- Para o backspace não enganchar
 end
 
@@ -24,6 +27,13 @@ function love.draw()
 	sceneManager:draw()
 end
 
+function love.resize(w, h)
+	-- Manter o tamanho mínimo da janela baseado na resolução atual em ui
+	-- if w < UI.UI_BASE_W or h < UI.UI_BASE_H then
+	-- 	love.window.setMode(UI.UI_BASE_W,UI.UI_BASE_H,{resizable=true,minwidth=UI.UI_BASE_W,minheight=UI.UI_BASE_H})
+	-- end
+end
+
 function love.keypressed(key)
 	sceneManager:keypressed(key)
 end
@@ -34,6 +44,10 @@ end
 
 function love.textinput(t)
 	sceneManager:textinput(t)
+end
+
+function love.wheelmoved(x, y)
+	sceneManager:wheelmoved(x, y)
 end
 
 function love.mousemoved(x, y, dx, dy, istouch)
